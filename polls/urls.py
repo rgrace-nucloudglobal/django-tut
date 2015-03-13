c0c8d7fa1243b22ea85acd74b59f160298342a4e
Richard Grace <rgrace@nucloudglobal.com>
2015-03-13 09:52:22 -0500

    Finished Tutorial 'Writing your first Django app, part 3'

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,9 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        # ex: /polls/5/ - regex capturing integer question_id
                        # parameter
-                       url(r'^(?P<question_id>\d+)/$',
-                           views.detail, name='detail'),
+                       url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
                        # ex: /polls/5/results/ - regex capturing a request for
                        # a given questions result page
-                       url(r'^(?P<question_id>\d+)/results/$',
-                           views.results, name='results'),
+                       url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
                        url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote')
                        )
