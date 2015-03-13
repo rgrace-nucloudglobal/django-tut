cabaeb6be041637905a51ee12fda1e47133655d0
Richard Grace <rgrace@nucloudglobal.com>
2015-03-13 07:42:47 -0500

    Using template system to draw detail.html

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http.response import HttpResponse, Http404
+from django.http.response import HttpResponse
+# , Http404
 from polls.models import Question
 from django.template import RequestContext, loader
 # Create your views here.
