23ee9b4fb2e24ce1045ed7454e24d2f97350fcf7
Richard Grace <rgrace@nucloudglobal.com>
2015-03-13 07:36:27 -0500

    Formatting & cleanup

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from polls import views
-urlpatterns = patterns('', 
+urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^(?P<question_id>\d+)/$', views.detail, name = 'detail'),
+                       url(r'^(?P<question_id>\d+)/$',
+                           views.detail, name='detail'),
                        )
--- polls/views.py
+++ polls/views.py
@@ -28,6 +28,6 @@ def detail(request, question_id):
     #         raise Http404('Question does not exist.')
     #     return render(request, 'polls/detail.html', {'question': question})
 
-    # shortcut to the abovedot
+    # shortcut to the above
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
